1e7e8effeae92d40c8471121813189a92c23cde0
SergeyRyzhov <serega.rf@gmail.com>
2014-03-19 22:09:00 +0400

    Small cleanup for usings.

--- ChipSynthesys/PlaceModel/Design.cs
+++ ChipSynthesys/PlaceModel/Design.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Soap;
 
 namespace PlaceModel
--- ChipSynthesys/PlaceModel/Models.cs
+++ ChipSynthesys/PlaceModel/Models.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace PlaceModel
+﻿namespace PlaceModel
 {
     /// <summary>
     /// Интерфейс алгоритмов глобального размещения компонентов интегральной схемы
