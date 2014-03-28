dd82b18e5822975ee3c7256befeb3bb41e788344
YuliaZamiatina <yulkazam@mail.ru>
2014-03-28 23:53:54 +0400

    CompontsOrderer.cs added

---
+++ ChipSynthesys/ChipSynthesys.UnitTests/PlaceModel/SortCompTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DetailPlacer.Algorithm;
+using PlaceModel;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ChipSynthesys.UnitTests.PlaceModel
+{
+    [TestClass]
+   public class SortCompTest
+    {
+        [TestMethod]
+        public void SortTests()
+        {
+            var order = new CompontsOrderer();
+            var p = new Component.Pool();
+            p.Add(3,3);
+            p.Add(3,3);
+            p.Add(5,3);
+            p.Add(2,3);
+            p.Add(3,2);
+            p.Add(2,2);
+            var n = new Net.Pool();
+            var design = new Design(new Field(0,0,20,20),p,n);
+            var perm = new int[6];
+            for (var i = 0; i < 6; i++) perm[i] = 0;
+            var placement= new PlacementGlobal(design);
+           
+            placement.x[design.components[0]] = 2;
+            placement.y[design.components[0]] = 2;
+            placement.x[design.components[1]] = 4;
+            placement.y[design.components[1]] = 4;
+            placement.x[design.components[2]] = 6;
+            placement.y[design.components[2]] = 6;
+            placement.x[design.components[3]] = 9;
+            placement.y[design.components[3]] = 2;
+            placement.x[design.components[4]] = 10;
+            placement.y[design.components[4]] = 1;
+            placement.x[design.components[5]] = 12;
+            placement.y[design.components[5]] = 5;
+           
+            
+            order.SortComponents(design,placement,new PlacementDetail(design),design.components,ref perm);
+            var res = new int[] { 1, 3, 5, 0, 2, 4 };
+            for (var i = 0; i < 6; i++)
+            {
+                Assert.AreEqual(res[i], perm[i]);
+            }
+         }
+
+    }
+}
--- ChipSynthesys/DetailPlacer.Algorithm/CompontsOrderer.cs
+++ ChipSynthesys/DetailPlacer.Algorithm/CompontsOrderer.cs
@@ -1,15 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using PlaceModel;
+
+
 
 namespace DetailPlacer.Algorithm
 {
     public class CompontsOrderer : ICompontsOrderer
     {
         public void SortComponents(Design design, PlacementGlobal approximate, PlacementDetail result, Component[] unplacedComponents, ref int[] perm)
-        {
-            for (int i = 0; i < unplacedComponents.Length; i++)
+        {            
+            var n = unplacedComponents.Count();
+            var Cols = new List<int>();
+            var RawIndex = new List<int>();
+            var indx = 0;
+
+            var degMas = new int[n];
+            RawIndex.Add(0);
+
+            for (var i = 0; i < n; i++)
             {
-                perm[i] = i;
+                var r1 = unplacedComponents[i];
+                var x1 = approximate.x[r1];
+                var y1 = approximate.y[r1];
+                var deg = 0;
+
+                for (var j = 0; j < n; j++)
+                {
+                    if (j == i) continue;
+                    var r2 = unplacedComponents[j];
+                    var x2 = approximate.x[r2];
+                    var y2 = approximate.y[r2];
+                    if (x1 + r1.sizex <= x2 || x2 + r2.sizex <= x1
+                        || y1 + r1.sizey <= y2 || y2 + r2.sizey <= y1) continue;
+                    Cols.Add(j);
+                    indx++;
+                    deg++;
+                }
+
+                RawIndex.Add(indx);
+                degMas[i] = deg;
             }
+            RawIndex.Add(Cols.Count());
+
+            var colors = new int[n];
+            for (var i = 0; i < n; i++) colors[i] = -1;
+
+            var maxDeg = degMas.Max();
+
+            do
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    if (degMas[i] != maxDeg) continue;
+                    var color = 0;
+                    for (var j = RawIndex[i]; j < RawIndex[i + 1]; j++)
+                    {
+                        if (colors[Cols[j]] == color)
+                        {
+                            color++;
+                            j = RawIndex[i];
+                        }
+                    }
+                    colors[i] = color;
+                }
+                maxDeg--;
+            } while (colors.Contains(-1));
+
+            var sortComp = new List<int>();
+            for (var i = 0; i <= colors.Max(); i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (colors[j] != i) continue;
+                    sortComp.Add(j);
+                }
+            }
+
+            perm = sortComp.ToArray();
+            
         }
     }
 }
