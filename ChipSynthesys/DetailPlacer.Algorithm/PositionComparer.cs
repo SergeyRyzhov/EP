3e096e2ffc32c18cac36a6fa4c3eaa32d95b2794
SergeyRyzhov <serega.rf@gmail.com>
2014-03-30 01:42:05 +0400

    Position comparer for square result.

--- ChipSynthesys/DetailPlacer.Algorithm/PositionComparer.cs
+++ ChipSynthesys/DetailPlacer.Algorithm/PositionComparer.cs
@@ -1,13 +1,25 @@
-﻿using PlaceModel;
+﻿using System;
+using PlaceModel;
 
 namespace DetailPlacer.Algorithm
 {
     public class PositionComparer : IPositionComparer
     {
+        private readonly Random m_rnd;
+
+        public PositionComparer()
+        {
+            m_rnd = new Random();
+        }
+
         public bool Better(Design design, PlacementDetail placement, Component current, int firstX, int firstY, int secondX, int secondY)
         {
             if (firstX < secondX)
                 if(firstY < secondY)
+                    return true;
+
+            if (firstX > secondX)
+                if (firstY > secondY)
                     return true;
             return false;
         }
