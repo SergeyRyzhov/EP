8261706d0a322a60ab9cbf1da96c520130663c7e
AnastasiaBorisovaB <aya9draga@mail.ru>
2014-04-06 19:29:40 +0400

    Генератор с добавлением пустой области

--- ChipSynthesys/ChipSynthesys/Generators/SquareGenerator.cs
+++ ChipSynthesys/ChipSynthesys/Generators/SquareGenerator.cs
@@ -2,6 +2,9 @@
 using PlaceModel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace ChipSynthesys.Common.Generators
 {
@@ -23,152 +26,133 @@ namespace ChipSynthesys.Common.Generators
         }
 
         public void NextDesignWithPlacement(int components, int nets, int maxNetSize, int percent, int maxSizeX, int maxSizeY, int width, int height, out Design design, out PlacementGlobal placement)
-        {
+        {      
            
-            var Parts = new List<int>();
-            var Coordinate = new List<int>();
-            var X_Coord = new List<int>();
-            var Y_Coord = new List<int>();
+            List<int> Parts = new List<int>();
+            List<int> Coordinate = new List<int>();
+            List<int> X_Coord = new List<int>();
+            List<int> Y_Coord = new List<int>();
             var rnd = new Random();
-            int SizeX, SizeY;
-            SizeX = components * maxSizeX;
-            SizeY = components * maxSizeY;
+            var SizeX=0;
+            var SizeY=0;
+            Square1(maxSizeX, maxSizeY, components, out SizeX, out SizeY);
             Division_Square(components,percent, SizeX, SizeY,maxSizeX,maxSizeY,Parts,Coordinate);  
             var c = new Component.Pool();
             var sum = 0;
-            var num=0;
-            var h=0;
-            var w = 0;
-            
-                for (var i = 0; i < Parts.Count - 1; i += 2)
+            var num=0;                       
+            for (int i = 0; i < Parts.Count - 1; i += 2)
                 {
                     c.Add(Parts[i] , Parts[i + 1]);
                     X_Coord.Add(Coordinate[i]);
                     Y_Coord.Add(Coordinate[i + 1]);
-                    sum += Parts[i] * Parts[i + 1];
-                    if (Coordinate[i] + Parts[i] > w)
-                    {w = Coordinate[i] + Parts[i]; }
-                    if (Coordinate[i + 1] + Parts[i + 1] > h)
-                    { h = Coordinate[i + 1] + Parts[i + 1]; }
+                    sum += Parts[i] * Parts[i + 1];                  
                     num = num + 1;
                     if (components == num)
                         break;
-                }
-          
-            Add_Square(w, h, sum, percent);
-            SizeX = w;
-            SizeY = h;
+                }      
+            sum = ((100 - percent) * sum) / percent;
+            Add_Square(SizeX, SizeY, sum, out SizeX,out SizeY);            
             var n = new Net.Pool();
             design = new Design(new Field(0, 0, SizeX, SizeY), c, n);
             placement = new PlacementGlobal(design);
-            var l=0;
-            foreach( var com in design.components)
+            int l=0;
+            foreach( Component com in design.components)
             {
                 placement.x[com] = X_Coord[l];
                 placement.y[com] = Y_Coord[l];
                 placement.placed[com] = true;
                 l++;
             }
-             
-            var Net = new List<int>();
-            var n_net = new List<int>();            
-            for (var k = 0; k < nets; k++)
-            {
-                var q = rnd.Next(components);
+            var q=0; 
+            List<int> Net = new List<int>();
+            List<int> n_net = new List<int>();            
+            for (int k = 0; k < nets; k++)
+            {               
                 Net.Add(q);                
-                var x_q = placement.x[c[q]];
-                var y_q = placement.y[c[q]];
-                var w_q = c[q].sizex;
-                var h_q = c[q].sizey;
-                var i=1;                
-                for (var j = 0; j < components; j++)
+                double x_q = placement.x[c[q]];
+                double y_q = placement.y[c[q]];
+                double w_q = c[q].sizex;
+                double h_q = c[q].sizey;
+                int i = 1;                
+                for (int j = 0; j < components; j++)
                 {
-                    var x_j = placement.x[c[j]];
-                    var y_j = placement.y[c[j]];
-                    var w_j = c[j].sizex;
-                    var h_j = c[j].sizey;
+                    double x_j = placement.x[c[j]];
+                    double y_j = placement.y[c[j]];
+                    int w_j = c[j].sizex;
+                    int h_j = c[j].sizey;
 
                     if ((x_q == x_j + w_j && y_q == y_j) || (x_q == x_j && y_q == y_j + h_j) || (x_q + w_q == x_j && y_q == y_j) || (x_q == x_j && y_q + h_q == y_j))
                     {
                         Net.Add(j);
+                        i++;                        
+                    }
+                    if ((x_j + w_j == x_q && y_j < y_q && y_q < y_j + h_j) || (x_j + w_j == x_q && y_q < y_j && y_j < y_q + h_q) || (x_q + w_q == x_j && y_j < y_q && y_q < y_j + h_j) || (x_q + w_q == x_j && y_q < y_j && y_j < y_q + h_q))
+                    {
+                        Net.Add(j);
                         i++;
                     }
-                    //if ((x_q == x_j + w_j && y_j < y_q && y_q < y_j + h_j) || (x_q == x_j + w_j && y_q < y_j && y_j < y_q + h_q) || (x_q + w_q == x_j && y_j < y_q && y_q < y_j + h_j) || (x_q + w_q == x_j && y_q <y_j && y_j < y_q + h_q))
-                    //{
-                    //    Net.Add(j);
-                    //    i++;
-                    //}
-                    //if ((y_q + h_q == y_j && x_j < x_q && x_q < x_j + w_j) || (y_q + h_q == y_j && x_q < x_j && x_j < x_q + w_q) || (y_q == y_j + h_j && x_j < x_q && x_q < x_j + w_j) || (y_q == y_j + h_j && x_q < x_j && x_j < x_q + w_q))
-                    //{
-                    //    Net.Add(j);
-                    //    i++;
-                    //}
+                    if ((y_j + h_j == y_q && x_j < x_q && x_q < w_j + x_j) || (y_j + h_j == y_q && x_q < x_j && x_j < x_q + w_q) || (y_q + h_q == y_j && x_j < x_q && x_q < w_j + x_j) || (y_q + h_q == y_j && x_q < x_j && x_j < x_q + w_q))
+                    {
+                        Net.Add(j);
+                        i++;
+                    }
                     if (i == maxNetSize || j==components-1)
                     { n_net.Add(i); break; }
-                }
-                
+                } 
+                q++;              
             }
             var count=0;
-            for (var i = 0; i < nets; i++)
+            for (int i = 0; i < nets; i++)
             {
                 
                 n.Add(new Component[n_net[i]]);
-                for (var j = 0; j <n[i].items.Length; j++)
+                for (int j = 0; j <n[i].items.Length; j++)
                 {
-                    var component = Net[count];
+                    int component = Net[count];
                       n[i].items[j] = c[component];
                       count++;                
                 }
             }
-
-
-            width = SizeX; //это кстати не прокатит для чисел они передаются по значению и не возвращаются  gоkjб[ратно
-            height = SizeY;
+            design = new Design(new Field(0, 0, SizeX, SizeY), c, n); 
+        
         }
 
-        private void Division_Square(int components, int percent,int SizeX, int SizeY, int maxSizeX, int maxSizeY, List<int> Parts, List<int> Coordinate)
-        {
-            int Square;          
-            Square = SizeX * SizeY ;           
-            Division(SizeX, SizeY, maxSizeX, maxSizeY,SizeX/2,SizeY/2, 0, Parts, Coordinate);
-            do
-            {
-                var i = Control_First(Parts, maxSizeX, maxSizeY);            
-                if ( i<0) break;                
-                if (Parts[i] == 1 || Parts[i + 1] == 1)
-                { break; }
-                else
-                { Division(Parts[i], Parts[i + 1], maxSizeX, maxSizeY, Parts[i] / 2, Parts[i + 1] / 2, i, Parts, Coordinate); }
-            } while(true);
-            do
-            {
 
-                var i = Contorol(Parts, maxSizeX, maxSizeY, components);
-                if (i < 0) break;
-                Division(Parts[i], Parts[i+1], maxSizeX, maxSizeY,maxSizeX,maxSizeY, i, Parts, Coordinate);
+        private void Division_Square(int components, int percent, int SizeX, int SizeY, int maxSizeX, int maxSizeY, List<int> Parts, List<int> Coordinate)
+        {
+            Division(SizeX, SizeY, maxSizeX, maxSizeY, 0, Parts, Coordinate);
+            do
+            {
+                int i = Contorol(Parts, maxSizeX, maxSizeY, components);
+                if (i == -1)
+                { break; }
+                Division(Parts[i], Parts[i + 1], maxSizeX, maxSizeY, i, Parts, Coordinate);
             } while (true);
 
         }
-
-        private void Division(int x, int y, int maxX, int maxY, int d_x, int d_y, int ind, List<int> Parts, List<int> Coordinate)
+        
+        private void Division(int x, int y, int maxX, int maxY, int ind, List<int> Parts, List<int> Coordinate)
         {
             int x1, x2, y1, y2;
-            var c_x = 0;
-            var c_y = 0;
-            var rnd = new Random();
+            int c_x = 0;
+            int c_y = 0;
+            int n = 0;
+            int k;
+            Random rnd = new Random();
 
-            if ((x > y && x>maxX) || (x==y && maxY > maxX )||(y==maxY && x>maxX) || ( y==1 && x>maxX))
+            if ((x > y) && x > 1 || (x == y && maxX <= maxY) || y == 1 || (x == y && maxX < x && maxY >= y))
             {
-                if (maxX > d_x && d_x!=1)
-                { d_x = maxX; }
-               
-                var n = 1 + rnd.Next(d_x);
+                if (x == maxX || x - maxX == 1)
+                {
+                    n = rnd.Next(1, maxX);                    
+                }
+                if (x <= maxX)
+                { n = rnd.Next(1, x); }
+                if (x - maxX > 1)
+                { n = rnd.Next(2, x - 1); }
+
                 x1 = n;
                 x2 = x - n;
-                if (x1 < 0 || x2 < 0 || x1==0 ||x2==0)
-                {
-                    var k = 21;
-                }
                 if (Parts.Count != 0)
                 {
                     c_y = Coordinate[ind + 1];
@@ -179,14 +163,14 @@ namespace ChipSynthesys.Common.Generators
                     Parts.Insert(ind + 1, y);
                     Parts.Insert(ind + 2, x2);
                     Parts.Insert(ind + 3, y);
-                    if (Coordinate.Count-1 == ind + 1)
+                    if (Coordinate.Count - 1 == ind + 1)
                     {
                         Coordinate.Add(c_x);
                         Coordinate.Add(c_y);
                     }
                     else
                     {
-                        Coordinate.Insert(ind + 2, c_x);                        
+                        Coordinate.Insert(ind + 2, c_x);
                         Coordinate.Insert(ind + 3, c_y);
                     }
                 }
@@ -205,17 +189,19 @@ namespace ChipSynthesys.Common.Generators
 
             }
 
-            if ((y > x&& y>maxY) || ( x==y && maxX > maxY) || ( x == maxX && y > maxY) || ( x==1 && y>maxY))
+            if (y > x || (x == y && maxY < maxX) || x == 1 || (x == y && maxY < y && maxX >= x))
             {
-                if (maxY > d_y && d_y != 1)
-                { d_y = maxY; }
-                var n = 1 + rnd.Next(d_y);
+                if (y == maxY || y - maxY == 1)
+                { n = rnd.Next(1, maxY); }
+                if (y <= maxY)
+                { n = rnd.Next(1, y); }
+                if (y - maxY > 1)
+                { n = rnd.Next(2, y - 1); }
+
                 y1 = n;
                 y2 = y - n;
-                if ( y1 <=0  || y2 <=0)
-                { var h=23;}
                 if (Parts.Count != 0)
-                {   
+                {
                     c_x = Coordinate[ind];
                     c_y = Coordinate[ind + 1] + n;
                     Parts.RemoveAt(ind + 1);
@@ -224,7 +210,7 @@ namespace ChipSynthesys.Common.Generators
                     Parts.Insert(ind + 1, y1);
                     Parts.Insert(ind + 2, x);
                     Parts.Insert(ind + 3, y2);
-                    if (Coordinate.Count-1 == ind + 1)
+                    if (Coordinate.Count - 1 == ind + 1)
                     {
                         Coordinate.Add(c_x);
                         Coordinate.Add(c_y);
@@ -237,93 +223,122 @@ namespace ChipSynthesys.Common.Generators
                 }
                 else
                 {
+                    Parts.Add(x);
+                    Coordinate.Add(c_x);
                     Parts.Add(y1);
-                    Coordinate.Add(c_x);
-                    Parts.Add(x);
                     Coordinate.Add(c_y);
-                    Parts.Add(y2);
-                    Coordinate.Add(c_x);
                     Parts.Add(x);
+                    Coordinate.Add(c_x);
+                    Parts.Add(y2);
                     Coordinate.Add(n);
                 }
             }
-          
+
 
         }
 
         private int Contorol(List<int> Parts, int maxX, int maxY, int comp)
         {
-            for (var i = 0; i < Parts.Count-1; i += 2)
+            int max;
+            int m;
+            List<int> M = new List<int>();
+            for (int i = 0; i < Parts.Count - 1; i += 2)
             {
                 if (Parts[i] > maxX || Parts[i + 1] > maxY)
                 {
+                    M.Clear();
                     return i;
                 }
-                if (Parts[i] <= maxX && Parts[i + 1] <=  maxY &&  comp != 0)
+                if (Parts[i] <= maxX && Parts[i + 1] <= maxY && comp != 0)
                 {
-                    comp = comp - 1;                    
+                    max = Parts[i] * Parts[i + 1];
+                    M.Add(max);
+                    comp--;
+                }
+                if (i == Parts.Count - 2 && comp != 0)
+                {
+                    m = M.Max();
+                    m = M.IndexOf(m) * 2;
+                    return m;
                 }
                 if (comp == 0)
                 { return -1; }
             }
             return -1;
-        }
+        }       
 
-        private int Control_First(List<int> Parts, int maxX, int maxY)
+        private void Add_Square(int w, int h, int sum, out int SizeX, out int SizeY)
         {
-            for (var i = 0; i < Parts.Count - 1; i+=2)
+            do
             {
-                if (Parts[i] * Parts[i + 1] > maxX * maxY * 2)
+                if (sum >= w + h)
                 {
-                    return i;
-                }                
-            }
-            return -1;
-        }
-
-        private void Add_Square(int w, int h, int sum, int percent)
-        {
-            int col;            
-            col = ((100 - percent) * sum) / percent;
-            col = col - (w * h - sum);// add square;
-            if (col > w && col > h)
-            {
-                if (col % w == 0 || col % h == 0)
-                {
-                    if (col % w == 0)
-                    {
-                        h = h + (col / w);
-                    }
-                    if (col % h == 0)
-                    {
-                        w = w + (col / h);
-                    }
+                    sum = sum - w;
+                    h++;
+                    sum = sum - h;
+                    w++;
                 }
                 else
                 {
-                    if (h < w)
-                    { Size(col, h, w); }
-                    if (w < h)
-                    { Size(col, w, h); }
+                    if (sum <= w || sum <= h)
+                    {
+                        if (sum <= w && w / 2 + 1 < sum)
+                        { sum = 0; h++; }
+                        if (sum <= h && h / 2 + 1 < sum)
+                        { sum = 0; w++; }
+                        else { sum = 0; }
+                    }
+                    else
+                    {
+                        if (w >= h)
+                        {
+                            sum = sum - w;
+                            h++;
+                            if (sum == w)
+                            { sum = 0; h++; }
+                            if (sum <= h && h / 2 + 1 < sum)
+                            { sum = 0; w++; }
+                            else { sum = 0; }
+                        }
+                        if (h > w && sum != 0)
+                        {
+                            sum = sum - h;
+                            w++;
+                            if (sum == h)
+                            { sum = 0; w++; }
+                            if (sum <= w && w / 2 + 1 < sum)
+                            { sum = 0; h++; }
+                            else { sum = 0; }
+                        }
+                    }
                 }
-            }
-            else
-            {
-                w = w - 0;
-                h = h - 0;
-            } 
-            
+            } while (sum != 0);
+            SizeX = w;
+            SizeY = h;
         }
 
-        private void Size(int col, int h, int w)
+        private void Square1(int X, int Y, int comp, out  int sizeX, out int sizeY)
         {
-         do
-                    {
-                        col = col - h;
-                        w = w + 1;
-                        if (col < h)
-                        { break; }                        
-                    } while (true);
-        }
+            int s = 0;
+            int i = 0;
+            int j = 0;
+            int n;
+            Random rnd = new Random();
+            do
+            {
+                i = rnd.Next(1, X + 1);
+                j = rnd.Next(1, Y + 1);
+                s = i * j + s;
+                comp--;
+                if (comp == 0)
+                { break; }
+            } while (true);
+            if (s % 2 != 0)
+            { s = s + 1; }
+
+            sizeX = Convert.ToInt16(Math.Sqrt(s));
+            sizeY = s / sizeX;
+        }          
+
     }
 }
